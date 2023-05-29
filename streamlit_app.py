64558260016e24bde30dcf2b9eaaafacb3d51de1
pickelmikel <88795708+pickelmikel@users.noreply.github.com>
2023-05-29 10:43:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ turns = (0)
 
 def yournum():
     global turns
-    ans = st.text_input("What is your number?: ", key = "ans")
+    ans = st.text_input("What is your number?: ", key = ans)
     turns += 1
     while turns < 3:
         if ans in ("q","quit"):
