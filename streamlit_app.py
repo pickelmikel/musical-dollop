e5dcdc3efb6080d326b13c36d33bc0d917703dbe
pickelmikel <88795708+pickelmikel@users.noreply.github.com>
2023-05-29 21:23:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,59 @@
 
+import streamlit as st
+from random import randint
+
+rnum_ = randint(0,255)
+yes = {"y","yes","yeah","ok","sure"}
+no = {"n","no","nope","bye"}
+welcome = "Welcome to this randow number generator, please pick a number between 0 and 255.\nHit q or type quit when you have had enough :)\n"
+turns = (0)
+
+def yournum():
+    global turns
+    ans = st.text_input("What is your number?: ")
+    turns += 1
+    while turns < 3:
+        if ans in ("q","quit"):
+            quit()
+        else:
+            try:
+                rnum(int(ans))
+            except:
+                st.write("Try a number...\n")
+                yournum()
+    else:
+        st.write("You are out of turns")
+        rs()
+
+def rnum(x):
+    if x > rnum_:
+        hi = x - rnum_
+        st.write("Your number is ", hi, " higher than the random number")
+    elif x < rnum_:
+        lo = rnum_ - x
+        st.write("Your number is ", lo, " lower than the random number")
+    elif x == rnum_:
+        st.write("Your number is exactly the random number, awesome!")
+        ta()
+    yournum()
+
+def ta():
+    ta_ = st.text_input("Do you want to try again?: ").lower()
+    if ta in yes:
+        rs()
+    elif ta in no:
+        quit()
+    else:
+        yournum()
+
+def rs():
+    global rnum 
+    global turns
+    rnum_ = randint(0,255)
+    turns = (0)
+    st.write(welcome)
+    yournum()
+
+#--- MAIN RUN CODE ---#
+st.write(welcome)
+yournum()
