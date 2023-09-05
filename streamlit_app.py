aa6e6817cbb80562bd0c430b790519406f53746e
pickelmikel <88795708+pickelmikel@users.noreply.github.com>
2023-09-05 13:34:57 -0500

    Update streamlit_app.py
    Showing Turns

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ rnum_ = randint(0,255)
 yes = {"y","yes","yeah","ok","sure"}
 no = {"n","no","nope","bye"}
 welcome = "Welcome to this random number generator, please pick a number between 0 and 255.\n" #Hit q or type quit when you have had enough :)\n"
-turns = (0)
+turns = []
 blank = ''
 
 def clear_text_input():
@@ -21,7 +21,7 @@ def yournum():
     try:
         rnum(int(ans))
     except ValueError:
-        st.write("Try a number...")
+        st.write(f"Try a number...{turns}: of turns")
 
 def rnum(x):
     global rnum_
